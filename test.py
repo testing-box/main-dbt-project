2f791cea0f63ad443a2c209e09dbde5e74376402
Adrian A <40632960+adrian5991@users.noreply.github.com>
2022-06-21 16:51:36 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+
+def test_dag() -> DAG:
+  with DAG(
+    dag_id="test",
+  ):
+    test_operator = KubernetesPodOperator(
+      namespace="namespace",
+      image="aws-image/test-application:v1.4",
+      get_logs=True,
+    )
+    
+    test_operator
+  return dag
